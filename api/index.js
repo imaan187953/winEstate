3eb726c9b5bebc9a387df68fd7e723c4050da077
Eman Sajjad <akzachoudhry1123@gmail.com>
2026-08-19 23:41:02 +0500

    mongoDB connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log("database connected successfully")
+}).catch((err) => {
+    console.log("connection failed", err);
+})
 
 const app = express();
 
